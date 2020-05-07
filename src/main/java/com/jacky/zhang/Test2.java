ff1fc0a1fdb6b57082e50de556e0549f5067193a
jacky <tigerzhang@quanyibao.com>
2020-05-07 15:03:02 +0800

    ss

--- src/main/java/com/jacky/zhang/Test2.java
+++ src/main/java/com/jacky/zhang/Test2.java
@@ -1,25 +1,26 @@
 package com.jacky.zhang;
 
+import java.util.*;
+
 public class Test2 {
-    public static boolean isPalindrome(int x) {
-        if(x>=0){
-            int s = x;
-            int res = 0;
-            while (x>0) {
-                int pop = x % 10;
-                res = res * 10 + pop;
-                x=x/10;
-            }
-            if(s==res){
-                return true;
-            }
-            return false;
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int len = nums.length;
+        Map<Integer, Set<Integer>> valueIndex = new HashMap<>(len); // 预存每个值与索引对应
+        for (int i = 0; i < len; i++) {
+            if (!valueIndex.containsKey(nums[i])) valueIndex.put(nums[i], new HashSet<>());
+            valueIndex.get(nums[i]).add(i);
         }
-        return false;
+        int[] sortedArr = Arrays.copyOf(nums, len), res = new int[len];
+        Arrays.sort(sortedArr); // 1, 2, 2, 3, 8
+        for (int si = len - 1; si >= 0; si--) { // 倒序，方便处理同值的情况
+            for (int i : valueIndex.get(sortedArr[si])) res[i] = si; // 同值的所有索引都更新
+        }
+        return res;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(Test2.isPalindrome(121));
+        int[] arr = new int[]{8,1,2,2,3};
+        System.out.println(Test2.smallerNumbersThanCurrent(arr));
     }
 }
---
+++ src/main/java/com/jacky/zhang/thread/LockSupportTest.java
@@ -0,0 +1,33 @@
+package com.jacky.zhang.thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class LockSupportTest {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+                if(i == 5){
+                    LockSupport.park();
+                }
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(8);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("after 8 seconds");
+        LockSupport.unpark(t);
+    }
+}
